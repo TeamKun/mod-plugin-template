04e1586f86bbbb956ccb368e7dcbee6601af2d91
Kotlin-Chan <developer.hazuku@gmail.com>
2021-05-01 00:18:54 +0900

    🎉 First Commit

---
+++ template-paper/src/main/java/net/kunmc/lab/template/TemplatePlugin.java
@@ -0,0 +1,17 @@
+package net.kunmc.lab.template;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TemplatePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
